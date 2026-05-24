dca4d5bdfb2124639b7b2823530d74fc30b79d9f
yhbwyb <yhbwyb@users.noreply.github.com>
2026-05-24 17:28:43 +0000

    deploy: 782da2b90540781cde9fcbcbe6dabe1782e168c5

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/money-tracker/sw.js', { scope: '/money-tracker/' })})}
